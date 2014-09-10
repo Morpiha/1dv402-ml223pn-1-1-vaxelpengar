44fb26eeb10f69e5a728f110926f98b4a522a2e6
morpiha <morpiha@live.se>
2014-09-10 15:05:59 +0200

    Commit 2

--- 1-1-vaxelpengar/Program.cs
+++ 1-1-vaxelpengar/Program.cs
@@ -10,24 +10,36 @@ namespace _1_1_vaxelpengar
     {
         static void Main(string[] args)
         {
-
+            //Anvgivna variable//
             double totalSum = 0;
             int givenSum = 0;
+            uint subtotal = 0;
+       
 
+            //Anvigen total summa och belopp//
             Console.Write("Ange totalsumma: ");
             totalSum = double.Parse(Console.ReadLine());
 
-            Console.Write("Erhållet belop: ");
+            Console.Write("Erhållet belopp: ");
             givenSum = int.Parse(Console.ReadLine());
 
+            //Kvitto//
             Console.WriteLine("\nKVITTO");
-            Console.WriteLine("----------------------------");
+            Console.WriteLine("---------------------------------");
 
-            Console.WriteLine("Totalt       :     {0:c}", totalSum);
-            totalSum = (uint)Math.Round(totalSum);
-            Console.WriteLine("Öresavrundning     :      ");
+            Console.WriteLine("Totalt              :   {0:c}", totalSum);
+            subtotal = (uint)Math.Round(totalSum);
+            double roundingOffAmount = totalSum - subtotal;
+            Console.WriteLine("Öresavrundning      :   {0:c}", roundingOffAmount);
+            Console.WriteLine("Att betala          :   {0:c}", subtotal);
+            Console.WriteLine("Kontant             :   {0:c}", givenSum);
 
-            Console.WriteLine("----------------------------");
+            double sumBack = givenSum - subtotal;
+            Console.WriteLine("Tillbaka            :   {0:c}", sumBack);
+
+            Console.WriteLine("---------------------------------");
+
+            
         }
     }
 }

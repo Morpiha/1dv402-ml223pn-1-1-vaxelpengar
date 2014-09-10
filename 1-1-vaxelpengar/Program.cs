20e785acde1f8c7dec7f962835444c0f170de7e5
morpiha <morpiha@live.se>
2014-09-10 17:18:09 +0200

    Commit 3

--- 1-1-vaxelpengar/Program.cs
+++ 1-1-vaxelpengar/Program.cs
@@ -14,12 +14,11 @@ namespace _1_1_vaxelpengar
             double totalSum = 0;
             int givenSum = 0;
             uint subtotal = 0;
-       
 
             //Anvigen total summa och belopp//
+
             Console.Write("Ange totalsumma: ");
             totalSum = double.Parse(Console.ReadLine());
-
             Console.Write("Erhållet belopp: ");
             givenSum = int.Parse(Console.ReadLine());
 
@@ -31,15 +30,23 @@ namespace _1_1_vaxelpengar
             subtotal = (uint)Math.Round(totalSum);
             double roundingOffAmount = totalSum - subtotal;
             Console.WriteLine("Öresavrundning      :   {0:c}", roundingOffAmount);
-            Console.WriteLine("Att betala          :   {0:c}", subtotal);
-            Console.WriteLine("Kontant             :   {0:c}", givenSum);
+            Console.WriteLine("Att betala          :   {0:c0}", subtotal);
+            Console.WriteLine("Kontant             :   {0:c0}", givenSum);
 
-            double sumBack = givenSum - subtotal;
-            Console.WriteLine("Tillbaka            :   {0:c}", sumBack);
+            double sumBack = givenSum - subtotal; 
+            Console.WriteLine("Tillbaka            :   {0:c0}", sumBack);
 
             Console.WriteLine("---------------------------------");
 
-            
+           
+            //Pengar att få tillbaks
+            int payBack= (int)Math.Round(sumBack);
+            int count = payBack/500;
+            int hundred = count/100;
+
+            Console.WriteLine("500-lappar          :   {0:0}", count);
+            Console.WriteLine("100-lappar          :   {0:0}", hundred);
+
         }
     }
 }

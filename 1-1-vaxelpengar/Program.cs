34def21046fa765b34c35f75e23c7ecf545a4ef1
unknown <ml223pn@NY108X07.lnu.se>
2014-09-10 14:26:55 +0200

    First commit

---
+++ 1-1-vaxelpengar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_vaxelpengar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalSum = 0;
+            int givenSum = 0;
+
+            Console.Write("Ange totalsumma: ");
+            totalSum = double.Parse(Console.ReadLine());
+
+            Console.Write("Erhållet belop: ");
+            givenSum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nKVITTO");
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("Totalt       :     {0:c}", totalSum);
+            totalSum = (uint)Math.Round(totalSum);
+            Console.WriteLine("Öresavrundning     :      ");
+
+            Console.WriteLine("----------------------------");
+        }
+    }
+}

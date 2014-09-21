e3824c36f60c123758fa16245b481203323da2fc
Morpiha <Morpiha@live.se>
2014-09-21 02:00:14 +0200

    Commit 4

--- 1-1-vaxelpengar/Program.cs
+++ 1-1-vaxelpengar/Program.cs
@@ -17,36 +17,92 @@ namespace _1_1_vaxelpengar
 
             //Anvigen total summa och belopp//
 
-            Console.Write("Ange totalsumma: ");
-            totalSum = double.Parse(Console.ReadLine());
-            Console.Write("Erhållet belopp: ");
-            givenSum = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Ange totalsumma: ");
+                    totalSum = double.Parse(Console.ReadLine());
 
-            //Kvitto//
-            Console.WriteLine("\nKVITTO");
-            Console.WriteLine("---------------------------------");
+                    if (totalSum < 1)
+                    {
+                        throw new OverflowException();
+                    }
+                    break;
+                }
+                catch(OverflowException)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nInmatning fel!");
+                    Console.ResetColor();
+                }
+                catch(FormatException)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nEtt oväntat fel har inträffat! Testa igen.");
+                    Console.ResetColor();
+                }
+            }
 
-            Console.WriteLine("Totalt              :   {0:c}", totalSum);
-            subtotal = (uint)Math.Round(totalSum);
-            double roundingOffAmount = totalSum - subtotal;
-            Console.WriteLine("Öresavrundning      :   {0:c}", roundingOffAmount);
-            Console.WriteLine("Att betala          :   {0:c0}", subtotal);
-            Console.WriteLine("Kontant             :   {0:c0}", givenSum);
 
-            double sumBack = givenSum - subtotal; 
-            Console.WriteLine("Tillbaka            :   {0:c0}", sumBack);
+            //Erhållet belopp
 
-            Console.WriteLine("---------------------------------");
+            while (true)
+            {
+                try
+                {
+                    Console.Write("\nErhållet belopp: ");
+                    givenSum = int.Parse(Console.ReadLine());
+                
+                
+                    if (givenSum < totalSum)
+                    {
+                        throw new OverflowException();
+                    }
+                    break;
+                }
+          
+                catch(OverflowException)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nFEL! Erhållet belopp felaktigt!");
+                    Console.ResetColor();
+                }
+                catch(FormatException)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nEtt onväntat fel har inträffat");
+                    Console.ResetColor();
+                }
+            }
 
-           
-            //Pengar att få tillbaks
-            int payBack= (int)Math.Round(sumBack);
-            int count = payBack/500;
-            int hundred = count/100;
 
-            Console.WriteLine("500-lappar          :   {0:0}", count);
-            Console.WriteLine("100-lappar          :   {0:0}", hundred);
 
+                //Kvitto//
+                Console.WriteLine("\nKVITTO");
+                Console.WriteLine("---------------------------------");
+
+                Console.WriteLine("Totalt              :   {0:c}", totalSum);
+                subtotal = (uint)Math.Round(totalSum);
+                double roundingOffAmount = totalSum - subtotal;
+                Console.WriteLine("Öresavrundning      :   {0:c}", roundingOffAmount);
+                Console.WriteLine("Att betala          :   {0:c0}", subtotal);
+                Console.WriteLine("Kontant             :   {0:c0}", givenSum);
+
+                double sumBack = givenSum - subtotal;
+                Console.WriteLine("Tillbaka            :   {0:c0}", sumBack);
+
+                Console.WriteLine("---------------------------------");
+
+
+                //Pengar att få tillbaks
+                int payBack = (int)Math.Round(sumBack);
+                int count = payBack / 500;
+                int hundred = count / 100;
+
+                Console.WriteLine("500-lappar          :   {0:0}", count);
+                Console.WriteLine("100-lappar          :   {0:0}", hundred);
+
+            }
         }
     }
-}
